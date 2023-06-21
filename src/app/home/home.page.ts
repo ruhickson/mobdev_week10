a18cfa4c0464d7a28b74be122dc0d2ffa9bd9286
Ru Hickson <ru.hickson@gmail.com>
2023-06-21 19:16:59 +0100

    Weather App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+interface WeatherResponse {
+  main: {
+    temp: number;
+    humidity: number;
+    pressure: number;
+  };
+  // Other properties from the response if needed
+}
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +21,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weatherValues :any
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
 
-  constructor() {}
-
+  loadData(){
+      this.httpClient.get<WeatherResponse>(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+        this.weatherValues = results.main
+    })
+  }
 }
